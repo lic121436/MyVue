b2c7d9715dd81a7489a10d7cb9d45ee588f262c9
lic121436 <lic121436@163.com>
2017-10-17 09:50:44 +0800

    增加路由

--- sell/src/main.js
+++ sell/src/main.js
@@ -3,13 +3,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
+import Seller from './components/seller/seller'
+import Goods from './components/goods/goods'
+import Ratings from './components/ratings/ratings'
+
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
+// 1. 定义（路由）组件。
+// 可以从其他文件 import 进来
+
+
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
+const routes = [
+  { path: '/seller', component: Seller },
+  { path: '/goods', component: Goods },
+  { path: '/ratings', component: Ratings }
+]
+
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+const app = new Vue({
+  router,
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
