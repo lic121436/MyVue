8391d615ddaa4428da83f6d0f002230d0d8eac6f
lic121436 <lic121436@163.com>
2017-10-17 16:15:53 +0800

    入口vue文件

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <div class="tab">
-      <router-link to="/seller">商家</router-link>
-      <router-link to="/goods">商品</router-link>
-      <router-link to="/ratings">评论</router-link>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
     <router-view></router-view>
   </div>
@@ -13,21 +19,51 @@
 <script>
 import vHeader from './components/header/header'
 
+const ERR_OK = 0;
 export default {
   name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this.axios.get('/api/seller').then((res) => {
+      var res = res.data;
+      if (res.errno === ERR_OK) {
+        this.seller = res.data;
+      }
+    })
+  },
   components: {
     vHeader
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .tab {
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    .tab-item {
+      flex: 1;
+      text-align: center;
+      &>a {
+        display: block;
+        font-size: 14px;
+        color: rgb(77, 85, 93);
+        &.active {
+          color: rgb(240, 20, 20);
+        }
+      }
+    }
+  }
 }
 </style>
