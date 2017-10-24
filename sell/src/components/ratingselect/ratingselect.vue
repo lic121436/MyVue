61fbb98b3e5ba597477fbf974dae760bed460f26
lic121436 <lic121436@163.com>
2017-10-24 17:59:51 +0800

    评论选择组件

--- sell/src/components/ratingselect/ratingselect.vue
+++ sell/src/components/ratingselect/ratingselect.vue
@@ -7,7 +7,7 @@
             <span class="block negative" :class="{'active': selectType === 1}">{{desc.negative}}<span class="count">4</span></span>
         </div>
         <!--  -->
-        <div class="switch">
+        <div class="switch" :class="{'on': onlyContent}">
             <i class="icon-check_circle"></i>
             <span class="text">只看有内容的评价</span>
         </div>
@@ -56,32 +56,53 @@ export default {
     padding: 18px 0;
     @include border-1px(rgba(7,17,27,0.1));
     .block {
-        display: inline-block;
-        padding: 8px 12px;
-        margin-right: 8px;
-        border-radius: 1px;
-        line-height: 16px;
-        font-size: 12px;
-        color: rgb(77,85,93);
-        &.active{
-            color: #fff;
+      display: inline-block;
+      padding: 8px 12px;
+      margin-right: 8px;
+      border-radius: 1px;
+      line-height: 16px;
+      font-size: 12px;
+      color: rgb(77, 85, 93);
+      &.active {
+        color: #fff;
+      }
+      .count {
+        margin-left: 2px;
+        font-size: 8px;
+      }
+      &.positve {
+        background-color: rgba(0, 160, 220, 0.2);
+        &.active {
+          background-color: rgb(0, 160, 220);
         }
-        .count{
-            margin-left: 2px;
-            font-size: 8px;
+      }
+      &.negative {
+        background-color: rgba(77, 85, 93, 0.2);
+        &.active {
+          background-color: rgb(77, 85, 93);
         }
-        &.positve{
-            background-color: rgba(0,160,220,.2);
-            &.active{
-                background-color: rgb(0,160,220);
-            }
-        }
-        &.negative{
-            background-color: rgba(77,85,93,.2);
-            &.active{
-               background-color: rgb(77,85,93); 
-            }
-        }
+      }
+    }
+  }
+  .switch {
+    padding: 12px 18px;
+    line-height: 24px;
+    font-size: 0;
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    color: rgb(147, 153, 159);
+    &.on {
+      .icon-check_circle {
+        color: #00c850;
+      }
+    }
+    .icon-check_circle {
+      display: inline-block;
+      margin-right: 4px;
+      font-size: 24px;
+      vertical-align: top;
+    }
+    .text {
+      font-size: 12px;
     }
   }
 }
