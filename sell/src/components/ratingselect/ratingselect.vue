2c7eac334bd6fbb4850d4468bc70071465f06230
lic121436 <lic121436@163.com>
2017-10-24 17:39:15 +0800

    选择评价组件

---
+++ sell/src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="ratingselect">
+        <!-- 评价类型 -->
+        <div class="rating-type border-1px">
+            <span class="block positve" :class="{'active': selectType === 2}">{{desc.all}}<span class="count">4</span></span>
+            <span class="block positve" :class="{'active': selectType === 0}">{{desc.positve}}<span class="count">4</span></span>
+            <span class="block negative" :class="{'active': selectType === 1}">{{desc.negative}}<span class="count">4</span></span>
+        </div>
+        <!--  -->
+        <div class="switch">
+            <i class="icon-check_circle"></i>
+            <span class="text">只看有内容的评价</span>
+        </div>
+    </div>
+</template>
+
+<script>
+const POSITVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    selectType: {
+      type: Number,
+      default: ALL
+    },
+    onlyContent: {
+      type: Boolean,
+      default: false
+    },
+    desc: {
+      type: Object,
+      default() {
+        return {
+          all: "全部",
+          positive: "满意",
+          negative: "不满意"
+        };
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../comnon/css/mixin.scss";
+.ratingselect {
+  .rating-type {
+    margin: 0 18px;
+    padding: 18px 0;
+    @include border-1px(rgba(7,17,27,0.1));
+    .block {
+        display: inline-block;
+        padding: 8px 12px;
+        margin-right: 8px;
+        border-radius: 1px;
+        line-height: 16px;
+        font-size: 12px;
+        color: rgb(77,85,93);
+        &.active{
+            color: #fff;
+        }
+        .count{
+            margin-left: 2px;
+            font-size: 8px;
+        }
+        &.positve{
+            background-color: rgba(0,160,220,.2);
+            &.active{
+                background-color: rgb(0,160,220);
+            }
+        }
+        &.negative{
+            background-color: rgba(77,85,93,.2);
+            &.active{
+               background-color: rgb(77,85,93); 
+            }
+        }
+    }
+  }
+}
+</style>
